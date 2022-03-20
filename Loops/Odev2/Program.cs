9f45b4dc732246ad7f8bd27e7a696adea3058193
Uğur Okan Çivgin <37384522+ugurcivgin34@users.noreply.github.com>
2022-03-20 16:36:28 +0300

    .

--- Loops/Odev2/Program.cs
+++ Loops/Odev2/Program.cs
@@ -6,7 +6,51 @@ namespace Odev2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //a - Kullanıcı sayı girecek.UYgulama bu sayının asal olup olmadığını söyleyecek.
+            Console.WriteLine("Bir sayı giriniz");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            int sayac = 0;
+
+            for (int i = 2; i < sayi; i++)
+            {
+                if (sayi % i == 0)
+                {
+                    sayac++;
+                }
+
+            }
+
+            if (sayac == 0)
+            {
+                Console.WriteLine(sayi + " sayi asal sayıdır");
+            }
+            else
+            {
+                Console.WriteLine(sayi + " sayi asal sayı değildir");
+            }
+
+            //--------------------------------------------------------------------------------------
+            // b - Birden 10.000 bine kadar tüm asal sayıları ekrana yazan bir uygulama.
+
+            sayac = 0;
+            for (int i = 2; i < 50; i++)
+            {
+                for (int j = 2; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        sayac++;
+                    }
+
+                }
+                if (sayac==0)
+                {
+                    Console.WriteLine(i);
+                }
+                sayac = 0;
+                
+            }
+
         }
     }
 }
