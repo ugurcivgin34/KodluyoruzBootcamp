a4a3563f542ea2b68448c06da6292ba6c9b5b6e2
Uğur Okan Çivgin <37384522+ugurcivgin34@users.noreply.github.com>
2022-03-20 16:21:38 +0300

    2.Ders İşlendi

--- introCSharp/Odev1/Program.cs
+++ introCSharp/Odev1/Program.cs
@@ -6,7 +6,18 @@ namespace Odev1
     {
         static void Main(string[] args)
         {
+            //ik kenarlarının uzunluğunu alan ve hipotenüsü hesaplayan programı yazın.
             
+            double c;
+
+            Console.Write("1. Kenarı Giriniz :");
+            int a = Convert.ToInt32(Console.ReadLine());
+        
+            Console.Write("2. Kenarı Giriniz :");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            c = Math.Sqrt((a * a) + (b * b));
+            Console.WriteLine("Hipotenüs : " + c);
         }
     }
 }
