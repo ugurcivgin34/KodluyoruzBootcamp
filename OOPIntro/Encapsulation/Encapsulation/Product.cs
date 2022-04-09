8fa29e1edbc78abd4d87d9834c2368665c3f8932
Uğur Okan Çivgin <37384522+ugurcivgin34@users.noreply.github.com>
2022-04-09 11:15:37 +0300

    .

---
+++ OOPIntro/Encapsulation/Encapsulation/Product.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    public class Product
+    {
+        private double _Price;
+        public void SetPrice(double value)
+        {
+            if (value>0)
+            {
+                _Price = value;
+            }
+            else
+            {
+                throw new ArgumentException("Fiyat negatif olamaz");
+            }
+        }
+
+        public double GetPrice()
+        {
+            return _Price;
+        }
+
+        private string _Name;
+        public string Name 
+        {
+            get { return _Name; }
+            set { _Name = value; }
+        }
+
+
+        private int _Stock;
+        public int StockCount
+        {
+            get
+            {
+                return _Stock;
+            }
+            set
+            {
+                if (value<0)
+                {
+                    _Stock = 0;
+                    IsproductFoundInStock = false;
+                }
+            }
+        }
+        public bool IsproductFoundInStock { get; private set; } //readonly , yazılması yani set olması sadece içerden oldu
+        public string Description { get; set; }
+    }
+}
---
+++ OOPIntro/Encapsulation/Encapsulation/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Encapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product kolye = new Product();
+            kolye.SetPrice(10);
+            Console.WriteLine(kolye.GetPrice());
+
+            kolye.Name = "Art 1";
+            kolye.StockCount = 1000;
+
+
+
+        }
+    }
+}
