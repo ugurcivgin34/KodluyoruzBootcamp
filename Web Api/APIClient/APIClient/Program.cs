6a7920b23976c7e1bed2530a92b4f35b422d16bb
Uğur Okan Çivgin <37384522+ugurcivgin34@users.noreply.github.com>
2022-04-17 15:55:04 +0300

    .

--- Web Api/APIClient/APIClient/Program.cs
+++ Web Api/APIClient/APIClient/Program.cs
@@ -11,24 +11,37 @@ namespace APIClient
     {
         static async Task  Main(string[] args)
         {
+            
             var client = new HttpClient();
-            var response =await client.GetAsync("https://localhost:7216/api/Products");
+            var response =await client.GetAsync("https://swapi.dev/api/planets/1");
             if (response.IsSuccessStatusCode)
             {
-            var product=await response.Content.ReadFromJsonAsync<List<Product>>();
+            var product=await response.Content.ReadFromJsonAsync<Planets>();
+                foreach (var item in product.Films)
+                {
+                    Console.WriteLine(item);
+                }
             }
 
         }
 
-        public class Product
+        public class Planets
         {
-            public int Id { get; set; }
             public string Name { get; set; }
-            public decimal? Price { get; set; }
-            public int? Stock { get; set; }
-            public string? Description { get; set; }
-            public double? Discount { get; set; }
-            public string? ImageUrl { get; set; }
+            public string Diameter { get; set; }
+            public string Rotation_Period { get; set; }
+            public string Orbital_Period { get; set; }
+            public string Gravity { get; set; }
+            public string Population { get; set; }
+            public string Climate { get; set; }
+            public string Terrain { get; set; }
+            public string Surface_Water { get; set; }
+            public string[] Residents { get; set; }
+            public string[] Films { get; set; }
+            public string Url { get; set; }
+            public string Created { get; set; }
+            public string Edited { get; set; }
+
 
 
 
---
+++ Web Api/Wired.com/Wired.com/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace Wired.com
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+           
+            var url = new Uri("https://www.wired.com/");
+            var client = new WebClient();
+            var html = client.DownloadString(url);
+            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+            doc.LoadHtml(html);
+           
+            var veri = doc.DocumentNode.SelectNodes("html/body/div[1]/div/main/div[1]/div[1]/section/div[3]/div/div/div/div/div/div/a");
+            foreach (var item in veri)
+            {
+                Console.WriteLine(item.InnerText +" ");
+                Console.WriteLine();
+            }
+        }
+    }
+}
