b8a9517a47d9f1416a7c94cdb5385bf5659c47d2
Uğur Okan Çivgin <37384522+ugurcivgin34@users.noreply.github.com>
2022-04-23 12:33:44 +0300

    Onion Architecture with API

---
+++ Web Api/Catalog.API/Catalog.API/Controllers/ProductsController.cs
@@ -0,0 +1,28 @@
+﻿using Catalog.Business;
+using Catalog.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService service;
+
+        //Asenkron zaman paylaşımlı , birbirinden bağımsız iş yapabilenler.Bir asenkron uygulama çalışırken başka bir uygulama 
+        //eş zamanlı olarak çalışabilir
+
+        public ProductsController(IProductService productService)
+        {
+            service = productService;     
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetProducts()
+        {
+            var products = await service.GetProducts();
+            return Ok(products);
+        }
+    }
+}
